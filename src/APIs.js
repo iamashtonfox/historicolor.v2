5a926b2e4bee19f1e03d2f6921895bb94a24c100
ashtonfox20 <ashfox@bu.edu>
2024-04-15 15:22:57 -0400

    minor tweak

--- src/APIs.js
+++ src/APIs.js
@@ -23,8 +23,8 @@ export async function getRandomArtworkUrl(fields = ["id", "title", "artist_id", 
     }
   }
 function getRandomInt(min, max) {
-    min = Math.ceil(min); // Round up to the nearest integer
-    max = Math.floor(max); // Round down to the nearest integer
+    min = Math.ceil(min);
+    max = Math.floor(max);
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
   
